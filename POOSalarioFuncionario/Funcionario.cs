94a887fc2b55a12f1f80bdc9809c49149534ecd9
yuzosergio <yuzosergio@hotmail.com>
2019-08-06 11:15:33 -0300

    final

---
+++ POOSalarioFuncionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace POOSalarioFuncionario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+        public double imposto;
+
+        public double SalComImp()
+        {
+            return salario - imposto;
+        }
+        public void Aumento(double porcentagem)
+        {
+                salario += (salario * porcentagem / 100.0);
+        }
+        public override string ToString()
+        {
+            return "Funcionario: "+nome + ", R$" + SalComImp().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ POOSalarioFuncionario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOSalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Digite o nome: ");
+            f.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salario: ");
+            f.salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o imposto: ");
+            f.imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(f);
+
+            Console.WriteLine("Digite a % para o aumento: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.Aumento(porcentagem);
+
+            Console.WriteLine("Dados atualizados: {0}",f);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
